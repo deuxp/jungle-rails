d54b8b330a0690dd16fc5a4e08b170c47d8ebe84
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-14 00:06:39 -0400

    dashboard tracks total products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,13 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PASSWORD"]   
 
   def show
+    # number of categories
+    @categories = Category.all.count
+    # sum of all products
+    @products = 0
+    # number of products of each category
+    Category.all.each do |category|
+      @products += category.products.count
+    end
   end
 end
