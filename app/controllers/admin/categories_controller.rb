b8dc81d431408b7ffb3ced49137a09a0fdd265e8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-14 22:43:57 -0400

    query all categories from db

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,16 @@
 class Admin::CategoriesController < ApplicationController
-    def index
+  http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PASSWORD"]
 
+    def index
+        @categories = Category.order(id: :asc).all
+    end
+
+    def new
+        raise "new Category Form"
+        # @category = Category.new    
+    end
+
+    def create
+        raise "you are saving your new category"
     end
 end
