5bc6e4605bdbc7c1e366ca3a4b5894ca893e120d
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-15 22:00:01 -0400

    sanitize params, create new user, set the session-cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # setting the session-cookie
+      session[:user_id] = user.id
+      redirect_to :root
+      else
+        render :new
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confrimation
+    )
   end
 end
