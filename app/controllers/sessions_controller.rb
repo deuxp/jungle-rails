aae7c78461794e43936d774014ae2bf3bba7ab6b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-16 02:36:37 -0400

    fix wrong redirect paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,23 @@
 class SessionsController < ApplicationController
-    
+    # GET /login
     def new
         # @session = Session.new
     end
     
+    # POST /login
     def create
         user = User.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
+            redirect_to :root
+        else
             redirect_to login_path
         end
     end
 
+    # GET /logout
     def destroy
         session[:user_id] = nil
-        redirect_to new_session_path
+        redirect_to :root
     end
 end
