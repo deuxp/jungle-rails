ced41c651b1521863657d0d51670ee7e36a41c03
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-18 01:06:29 -0400

    validate first_name exists; last_name exitst

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     # authentication method via bcrypt
     has_secure_password
+    validates_presence_of :first_name
+    validates_presence_of :last_name
     validates_presence_of :email
     validates_uniqueness_of :email
 end
