7c1b85630a6294c5a63426a1e73f2f8dff1d1a76
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-18 10:33:34 -0400

    fix login_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to new_session_path unless current_user
+    redirect_to login_path unless current_user
   end
 
   def cart
