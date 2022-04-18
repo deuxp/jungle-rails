1c6420575d87ce77d2b0464ad27c4543e4a21027
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-17 21:37:10 -0400

    new:get image; create: refactor to proper login_path for params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
     # GET /login
     def new
+    @image = Product.where(id: 11)[0].image
     end
     
     # POST /login
     def create
-        user = User.find_by_email(params[:email])
-        if user && user.authenticate(params[:password])
+        user = User.find_by_email(params[login_path][:email])
+        if user && user.authenticate(params[login_path][:password])
             session[:user_id] = user.id
             redirect_to :root
         else
