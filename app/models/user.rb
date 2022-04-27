79bb5312d24279e0b764eb219daaaec52452385a
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 12:42:44 -0400

    validate presence of password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
     has_secure_password
     validates_presence_of :first_name
     validates_presence_of :last_name
+    validates_presence_of :password_confirmation
     validates_presence_of :email
-    validates_uniqueness_of :email
+    validates_uniqueness_of :email, case_sensitive: false
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
