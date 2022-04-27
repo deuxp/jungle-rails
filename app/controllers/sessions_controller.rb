a39f778f948d7d49eb0f3c430e3f86b264c4cc14
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 16:03:58 -0400

    refactor create: authenticate_with_user_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
     
     # POST /login
     def create
-        @user = User.find_by_email(params[login_path][:email])
-        if @user && @user.authenticate(params[login_path][:password])
-            session[:user_id] = @user.id
+        if user = User.authenticate_with_credentials(params[login_path][:email], params[login_path][:password])
+            session[:user_id] = user.id
             redirect_to :root
         else
+            # raise "#{user.inspect}"
             redirect_to login_path, notice: 'Invaild email/password credentials'
         end
     end
