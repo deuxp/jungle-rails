f1a33ae00b738b37f50bda53b87eb3cbc4d2e96d
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 16:03:04 -0400

    implement authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
     # authentication method via bcrypt
     has_secure_password
+    validates_presence_of :password_confirmation
+    validates :password, length: { in: 6..72 }
     validates_presence_of :first_name
     validates_presence_of :last_name
-    validates_presence_of :password_confirmation
     validates_presence_of :email
     validates_uniqueness_of :email, case_sensitive: false
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    
+    def self.authenticate_with_credentials email, password
+        # find the User first User.find_by(email: email)
+        # User.authenticate(password) -> evaluates to true or false
+        self.find_by(email: email).try(:authenticate, password)
+    end
 end
