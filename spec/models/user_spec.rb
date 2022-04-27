6477e8be072e1f2e209702f1146e207e1a19faeb
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 12:41:37 -0400

    test examples for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,173 @@
 require 'rails_helper'
-# finder
+
+#login Act
+#login = User.find_by(email: 'ww@qmail.com').try(:authenticate, 'ww0000')
+
 
 RSpec.describe User, type: :model do
-  
-  before(:each) do
-    # One persistent user entry
+
+  # Persistent user to compare uneiqueness
+  before(:all) do
+    @unique_user = User.new({
+      first_name: 'Jesse',
+      last_name: 'Pinkman',
+      email: 'jp@qmail.com',
+      password: 'jp0000',
+      password_confirmation: 'jp0000'
+      })
+    @unique_user.save
+  end
+    
+    after(:each) do
+      @user.destroy
+    end
+
+  # -------------------------- end of setup tear-down -------------------------- #
+    
+  it "should save a user with proper form entry" do
     @user = User.new({
       first_name: 'Walter',
       last_name: 'White',
       email: 'ww@qmail.com',
       password: 'ww0000',
       password_confirmation: 'ww0000'
-      })
-      @successful_entry = @user.save
-    end
-    
-    after(:each) do
-      @user.destroy
-    end
-    
-    it "should have one user" do
-      expect(@successful_entry).to be true
-    end
-    
-    context ":password" do
-      
-      it 'should have a password' do
-        login = User.find_by(email: 'ww@qmail.com').try(:authenticate, 'ww0000')
-        # puts login["password_digest"].length.inspect
-        expect(login[:password_digest]).to exist
-      end
-
-    # it should have a password_confirmation
-    # it should have identical password and password_confirmation fields
-  end
-
-  context ":email" do
-    # it should have an email
-    # it should have a unique email address, case insensitive
-  end
-
-  context "User Name" do
-    # it should have a first_name
-    # it should have a last_name
+    })
+    @successful_entry = @user.save
+    expect(@successful_entry).to be true
   end
   
+  # ----------------------------- Context: PASSWORD ---------------------------- #
+  context ":password" do
+    
+    it 'should reject if the password field is empty' do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'ww@qmail.com',
+        password_confirmation: 'ww0000'
+      })
+      save = @user.save
+      errors = @user.errors.full_messages
+
+      expect(save).to be false
+      expect(errors).to include("Password can't be blank")
+    end
+
+    it "should reject if the password_confirmation is empty" do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'ww@qmail.com',
+        password: 'ww0000'
+      })
+      save = @user.save
+      errors = @user.errors.full_messages
+
+      expect(save).to be false
+      expect(errors).to include("Password confirmation can't be blank")
+    end
+
+    it "should reject if the password_confirmation and password don't match" do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'ww@qmail.com',
+        password: 'ww0000',
+        password_confirmation: '0000'
+      })
+      save = @user.save
+      errors = @user.errors.full_messages
+
+      expect(save).to be false
+      expect(errors).to include("Password confirmation doesn't match Password")
+    end
+    
+
+      
+  end
+    
+    # ------------------------------ context: EMAIL ------------------------------ #
+  context ":email" do
+    
+    it 'should not accept blank email' do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        password: 'ww0000',
+        password_confirmation: 'ww0000'
+        })
+      @user.save
+      errors = @user.errors.full_messages
+      expect(errors).to include("Email can't be blank")
+        
+    end
+    
+    it 'should accept a email of the correct format only' do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'wwqmail.com',
+        password: 'ww0000',
+        password_confirmation: 'ww0000'
+        })
+      @successful_entry = @user.save          
+      errors = @user.errors.full_messages
+      expect(errors).to include("Email is invalid")
+    end
+      
+    it 'should have a unique email address' do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'jp@qmail.com',
+        password: 'ww0000',
+        password_confirmation: 'ww0000'
+      })
+      @user.save          
+      errors = @user.errors.full_messages
+      expect(errors).to include("Email has already been taken")
+    end
+    
+    it 'should have a unique email address while case insensitive' do
+      @user = User.new({
+        first_name: 'Walter',
+        last_name: 'White',
+        email: 'JP@qmail.com',
+        password: 'ww0000',
+        password_confirmation: 'ww0000'
+      })
+        @user.save          
+        errors = @user.errors.full_messages
+        expect(errors).to include("Email has already been taken")
+      end
+      
+    end
+      
+      # ---------------------------- context: USER NAME ---------------------------- #
+    context "User Name" do
+      it 'should have a first_name' do
+        @user = User.new({
+          last_name: 'White',
+          email: 'ww@qmail.com',
+          password: 'ww0000',
+          password_confirmation: 'ww0000'
+        })
+        @user.save          
+        errors = @user.errors.full_messages
+        expect(errors).to include("First name can't be blank")
+      end
+      it 'should have a last_name' do
+        @user = User.new({
+          first_name: 'Walter',
+          email: 'ww@qmail.com',
+          password: 'ww0000',
+          password_confirmation: 'ww0000'
+        })
+        @user.save          
+        errors = @user.errors.full_messages
+        expect(errors).to include("Last name can't be blank")
+      end
+    end
+      
 end
+        
