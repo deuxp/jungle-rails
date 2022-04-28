3b7efe12005fc5889bd65488266192ed7571cec0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 20:04:36 -0400

    downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ActiveRecord::Base
     validates_presence_of :email
     validates_uniqueness_of :email, case_sensitive: false
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    before_save { |user| user.email = email.downcase}
     
     def self.authenticate_with_credentials email, password
         # find the User first User.find_by(email: email)
         # User.authenticate(password) -> evaluates to true or false
-        self.find_by(email: email.downcase).try(:authenticate, password)
+        self.find_by(email: email.downcase.strip).try(:authenticate, password)
     end
 end
