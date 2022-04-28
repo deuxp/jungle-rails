fdaea9aac7a43c097a69b0d0c0d8683bb9d6d264
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-28 00:09:11 -0400

    check for css when page is loaded

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They see all products" do
-    #ACT
+    # ------------------------------------ ACT ----------------------------------- #
     visit root_path
 
-    #DEBUG / VERIFY
+    # ------------------------------ DEBUG / VERIFY ------------------------------ #
     save_screenshot
+
+    # ---------------------------- CHECK for PRODUCTS ---------------------------- #
+    expect(page).to have_css 'article.product' 
+    
   end
 end
