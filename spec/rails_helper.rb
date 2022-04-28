b69eb4a80cc2e2e3fda0088f7a48ee509f1980c5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-27 23:51:00 -0400

    requires in the wrong location in the file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'capybara/rails'
-require 'capybara/rspec'
-require 'capybara/poltergeist'
 ENV['RAILS_ENV'] ||= 'test'
 
-Capybara.javascript_driver = :poltergeist
 
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -65,4 +64,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Capybara.javascript_driver = :poltergeist
 end
